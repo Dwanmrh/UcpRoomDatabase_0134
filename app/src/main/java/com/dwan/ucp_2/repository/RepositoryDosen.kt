735d188ac36043b711bf8c615d27fd26fe2b4fc9
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-16 17:28:00 +0700

    Membuat operasi untuk create dan read di RepositoryDosen

---
+++ app/src/main/java/com/dwan/ucp_2/repository/RepositoryDosen.kt
@@ -0,0 +1,15 @@
+package com.dwan.ucp_2.repository
+
+import com.dwan.ucp_2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryDosen {
+
+    suspend fun insertDosen(dosen: Dosen)
+
+    // getAllDosen
+    fun getAllDosen(): Flow<List<Dosen>>
+
+    // getDosen
+    fun getDosen(): Flow<Dosen>
+}
