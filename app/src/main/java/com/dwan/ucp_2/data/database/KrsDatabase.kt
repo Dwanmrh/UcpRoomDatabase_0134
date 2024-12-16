6cc8219dafa487e42a3d26822b9e12133bcc0fd9
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-16 17:12:44 +0700

    Mendefinisikan fungsi untuk mengakses data dosen dan data mata kuliah

--- app/src/main/java/com/dwan/ucp_2/data/database/KrsDatabase.kt
+++ app/src/main/java/com/dwan/ucp_2/data/database/KrsDatabase.kt
@@ -3,9 +3,13 @@ package com.dwan.ucp_2.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.dwan.ucp_2.data.dao.DosenDao
+import com.dwan.ucp_2.data.dao.MataKuliahDao
 import com.dwan.ucp_2.data.entity.Dosen
 import com.dwan.ucp_2.data.entity.MataKuliah
 
 @Database(entities = [Dosen::class], [MataKuliah::class], version = 1, exportSchema = false)
 abstract class KrsDatabase: RoomDatabase() {
+    abstract fun dosenDao(): DosenDao
+    abstract fun mataKuliahDao(): MataKuliahDao
+
 }
