9f2a1e42442632c587af22458e5bcfb5265e3fb4
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-16 17:19:09 +0700

    Meengecek apakah Instance sudah dibuat sebelumnya. Membuat database baru dengan nama KrsDatabase

--- app/src/main/java/com/dwan/ucp_2/data/database/KrsDatabase.kt
+++ app/src/main/java/com/dwan/ucp_2/data/database/KrsDatabase.kt
@@ -1,15 +1,34 @@
 package com.dwan.ucp_2.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.dwan.ucp_2.data.dao.DosenDao
 import com.dwan.ucp_2.data.dao.MataKuliahDao
 import com.dwan.ucp_2.data.entity.Dosen
 import com.dwan.ucp_2.data.entity.MataKuliah
+import kotlin.concurrent.Volatile
 
 @Database(entities = [Dosen::class], [MataKuliah::class], version = 1, exportSchema = false)
 abstract class KrsDatabase: RoomDatabase() {
     abstract fun dosenDao(): DosenDao
     abstract fun mataKuliahDao(): MataKuliahDao
 
+    companion object {
+        @Volatile
+        private var Instance: KrsDatabase? = null
+
+        fun getDatabase(context: Context): KrsDatabase {
+            return(Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    KrsDatabase::class.java,
+                    name ="KrsDatabase"
+                )
+                    .build().also { Instance = it }
+            })
+        }
+
+    }
 }
