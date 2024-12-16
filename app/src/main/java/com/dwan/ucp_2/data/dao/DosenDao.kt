a87baad50a64263e9dce179b57b94bae9edba916
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-16 17:33:10 +0700

    Membuat operasi untuk create dan read di DosenDao

--- app/src/main/java/com/dwan/ucp_2/data/dao/DosenDao.kt
+++ app/src/main/java/com/dwan/ucp_2/data/dao/DosenDao.kt
@@ -1,7 +1,23 @@
 package com.dwan.ucp_2.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.dwan.ucp_2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface DosenDao {
+
+    // get all data
+    @Query("SELECT * FROM dosen")
+    fun getAllDosen(): Flow<List<Dosen>>
+
+    // get dosen
+    @Query("SELECT * FROM dosen WHERE nidn = :nidn")
+    fun getDosen(nidn: String): Flow<Dosen>
+
+    // create dosen
+    @Insert
+    suspend fun insertDosen(dosen: Dosen)
 }
