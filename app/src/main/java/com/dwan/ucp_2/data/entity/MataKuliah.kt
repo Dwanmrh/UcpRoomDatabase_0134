f869b2d644abff1e1d1b44a996fdd0e4ed7341a7
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-16 16:59:47 +0700

    Membuat tabel MataKuliah dengan atribut nya

---
+++ app/src/main/java/com/dwan/ucp_2/data/entity/MataKuliah.kt
@@ -0,0 +1,15 @@
+package com.dwan.ucp_2.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "MataKuliah")
+data class MataKuliah(
+    @PrimaryKey
+    val kode: String,
+    val namaMk: String,
+    val sks: String,
+    val semester: String,
+    val jenis: String,
+    val dosenPu: String
+)
