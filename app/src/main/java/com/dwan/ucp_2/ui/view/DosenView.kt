1afc364ba7bdd586c90bc6d8e07825c5c4c74e96
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-18 18:37:12 +0700

    Membuat card untuk dosen

--- app/src/main/java/com/dwan/ucp_2/ui/view/DosenView.kt
+++ app/src/main/java/com/dwan/ucp_2/ui/view/DosenView.kt
@@ -1,16 +1,28 @@
 package com.dwan.ucp_2.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.dwan.ucp_2.data.entity.Dosen
 import com.dwan.ucp_2.ui.customwidget.AppBar
 import com.dwan.ucp_2.ui.viewmodel.DosenViewModel
 import com.dwan.ucp_2.ui.viewmodel.PenyediaViewModel
@@ -48,3 +60,58 @@ fun DosenView(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun cardDosen(
+    dsn: Dosen,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    Card(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(7.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(7.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(5.dp))
+                Text(
+                    text = dsn.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(5.dp))
+                Text(
+                    text = dsn.nidn,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(5.dp))
+                Text(
+                    text = dsn.jenisKelamin,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+        }
+    }
+}
