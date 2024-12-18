cb96485a9e945d20cdd6f65c1fc105937f0fb2f6
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-18 17:13:01 +0700

    Mendeklarasikan containerApp di KrsApp

--- app/src/main/java/com/dwan/ucp_2/KrsApp.kt
+++ app/src/main/java/com/dwan/ucp_2/KrsApp.kt
@@ -1,7 +1,15 @@
 package com.dwan.ucp_2
 
 import android.app.Application
+import com.dwan.ucp_2.dependenciesinjection.ContainerApp
+import com.dwan.ucp_2.dependenciesinjection.InterfaceContainerApp
 
 class KrsApp: Application() {
+    lateinit var containerApp: ContainerApp
 
+    override fun onCreate() {
+        super.onCreate()
+
+        containerApp = ContainerApp(this)
+    }
 }
