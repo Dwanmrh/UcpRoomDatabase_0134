62c99f84fff6d4693d4414a829d6d76d094b9cc9
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-18 17:02:33 +0700

    Membuat Container App untuk dosen dan mata kuliah

--- app/src/main/java/com/dwan/ucp_2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/dwan/ucp_2/dependenciesinjection/ContainerApp.kt
@@ -1,5 +1,22 @@
 package com.dwan.ucp_2.dependenciesinjection
 
-interface InterfaceContainerApp {
+import android.content.Context
+import com.dwan.ucp_2.data.database.KrsDatabase
+import com.dwan.ucp_2.repository.LocalRepositoryDosen
+import com.dwan.ucp_2.repository.LocalRepositoryMataKuliah
+import com.dwan.ucp_2.repository.RepositoryDosen
+import com.dwan.ucp_2.repository.RepositoryMataKuliah
 
+interface InterfaceContainerApp {
+    val repositoryDosen: RepositoryDosen
+    val repositoryMataKuliah: RepositoryMataKuliah
+}
+
+class ContainerApp(private val context: Context): InterfaceContainerApp {
+    override val repositoryDosen: RepositoryDosen by lazy {
+        LocalRepositoryDosen(KrsDatabase.getDatabase(context).dosenDao())
+    }
+    override val repositoryMataKuliah: RepositoryMataKuliah by lazy {
+        LocalRepositoryMataKuliah(KrsDatabase.getDatabase(context).mataKuliahDao())
+    }
 }
