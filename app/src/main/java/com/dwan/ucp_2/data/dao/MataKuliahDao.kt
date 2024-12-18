9a4e434a49e21ec71270be199c02270e2847eace
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-18 16:15:47 +0700

    Membuat operasi crud di MataKuliahDao

--- app/src/main/java/com/dwan/ucp_2/data/dao/MataKuliahDao.kt
+++ app/src/main/java/com/dwan/ucp_2/data/dao/MataKuliahDao.kt
@@ -1,7 +1,28 @@
 package com.dwan.ucp_2.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Update
+import com.dwan.ucp_2.data.entity.MataKuliah
+import java.util.concurrent.Flow
 
 @Dao
 interface MataKuliahDao {
+
+    // get all data
+    @Query("SELECT * FROM matakuliah")
+    fun getAllMk(): Flow<List<MataKuliah>>
+
+    // get Mk
+    @Query("SELECT * FROM matakuliah WHERE kodeMk = :kodeMk")
+    fun getMk(kodeMk: String): Flow<MataKuliah>
+
+    // delete Mk
+    @Delete
+    suspend fun deleteMk(mataKuliah: MataKuliah)
+
+    // update Mk
+    @Update
+    suspend fun updateMk(mataKuliah: MataKuliah)
 }
