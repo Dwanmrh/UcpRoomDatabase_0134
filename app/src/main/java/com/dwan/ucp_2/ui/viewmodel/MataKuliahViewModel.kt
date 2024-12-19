031b0f5dcaae6d2614852a0644750be3b685e4ee
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-19 12:19:49 +0700

    Membuat formErrorState untuk mata kuliah

--- app/src/main/java/com/dwan/ucp_2/ui/viewmodel/MataKuliahViewModel.kt
+++ app/src/main/java/com/dwan/ucp_2/ui/viewmodel/MataKuliahViewModel.kt
@@ -9,6 +9,20 @@ class MataKuliahViewModel(private val repositoryMataKuliah: RepositoryMataKuliah
 
 }
 
+data class FormErrorStateMk(
+    val kodeMk: String? = null,
+    val namaMk: String? = null,
+    val sks: String? = null,
+    val semester: String? = null,
+    val jenis: String? = null,
+    val dosenPu: String? = null
+) {
+    fun isValid(): Boolean {
+        return kodeMk == null && namaMk == null && sks == null && semester == null
+                && jenis == null && dosenPu == null
+    }
+}
+
 fun MataKuliahEvent.toMataKuliahEntity(): MataKuliah = MataKuliah(
     kodeMk = kodeMk,
     namaMk = namaMk,
