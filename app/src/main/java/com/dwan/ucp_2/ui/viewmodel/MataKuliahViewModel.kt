92dc820bd6ed8237e24318579cbea11b372188a3
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-19 12:52:31 +0700

    Membuat state untuk mengelola proses loading, success, dan error saat mengambil daftar mata kuliah dari repository dan meneruskannya ke UI

--- app/src/main/java/com/dwan/ucp_2/ui/viewmodel/MataKuliahViewModel.kt
+++ app/src/main/java/com/dwan/ucp_2/ui/viewmodel/MataKuliahViewModel.kt
@@ -1,13 +1,54 @@
 package com.dwan.ucp_2.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.dwan.ucp_2.data.entity.Dosen
 import com.dwan.ucp_2.data.entity.MataKuliah
 import com.dwan.ucp_2.repository.RepositoryDosen
 import com.dwan.ucp_2.repository.RepositoryMataKuliah
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class MataKuliahViewModel(private val repositoryMataKuliah: RepositoryMataKuliah) : ViewModel() {
 
+    var uiState by mutableStateOf(MkUiState())
+    val MkUiState: StateFlow<MkUiState> = repositoryMataKuliah.getAllMk()
+        .filterNotNull()
+        .map {
+            MkUiState(
+                listDsn = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(MkUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                MkUiState(
+                    isLoading =false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = MkUiState(
+                isLoading = true,
+            )
+        )
 }
 
 data class MkUiState(
