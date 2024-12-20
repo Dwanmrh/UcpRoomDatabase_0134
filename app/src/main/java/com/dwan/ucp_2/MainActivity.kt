87049dad7990c366b8898398cdf3538cd6e13094
Ridwan Hidayatullah <ridwanhh10@gmail.com>
2024-12-20 15:37:41 +0700

    Menyesuaikan pemanggilan di Main Activity

--- app/src/main/java/com/dwan/ucp_2/MainActivity.kt
+++ app/src/main/java/com/dwan/ucp_2/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.dwan.ucp_2.ui.navigation.PengelolaHalaman
 import com.dwan.ucp_2.ui.theme.UCP_2Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             UCP_2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PengelolaHalaman(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
